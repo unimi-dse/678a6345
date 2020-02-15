f48f76b8c34f6f2b5325ea0fb2616311ad803b82
andbarbat <59977111+andbarbat@users.noreply.github.com>
2020-02-15 10:29:23 +0100

    b

--- R/data.R
+++ R/data.R
@@ -1,6 +1,3 @@
-library(plotly)
-library(quantmod)
-
 #' Data acquisition
 #' @param data_acquisition data acquisition, cod stocks: AAPL, source: yahoo
 #' @return total storic trend of Apple stock (source of data Yahoo Date AAPL.Open AAPL.High AAPL.Low AAPL.Close AAPL.Volume AAPL.Adjusted)
